5d80742011a9ad582f863f49231fd253a1e4dfe1
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-12-31 12:05:49 +0200

    setup navbar context

--- src/context/context.js
+++ src/context/context.js
@@ -3,9 +3,39 @@ import React, { Component } from "react";
 const ProductContext = React.createContext();
 
 class ProductProvider extends Component {
+  state = {
+    sidebarOpen: false,
+    cartOpen: false,
+    cartItems: 0
+  };
+
+  handleSidebar = () => {
+    this.setState({ sidebarOpen: !this.state.sidebarOpen });
+  };
+
+  handleCart = () => {
+    this.setState({ cartOpen: !this.state.cartOpen });
+  };
+
+  openCart = () => {
+    this.setState({ cartOpen: true });
+  };
+
+  closeCart = () => {
+    this.setState({ cartOpen: false });
+  };
+
   render() {
     return (
-      <ProductContext.Provider value="context">
+      <ProductContext.Provider
+        value={{
+          ...this.state,
+          handleCart: this.handleCart,
+          handleSidebar: this.handleSidebar,
+          openCart: this.openCart,
+          closeCart: this.closeCart
+        }}
+      >
         {this.props.children}
       </ProductContext.Provider>
     );
