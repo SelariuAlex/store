da721015eb7571618a003beffa8477f18a3a2905
Selariu Alexandru <Selalex_gd@yahoo.com>
2020-01-02 11:53:45 +0200

    sidebar component

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,5 +1,50 @@
 import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { ProductConsumer } from "../context";
 
 export const Sidebar = () => {
-  return <div>sidebar</div>;
+  return (
+    <ProductConsumer>
+      {value => {
+        const { links, sidebarOpen, handleSidebar } = value;
+        return (
+          <SideWrapper>
+            <ul>
+              {links.map(link => {
+                return (
+                  <li key={link.id}>
+                    <Link
+                      to={link.path}
+                      className="sidebar-link"
+                      onClick={handleSidebar}
+                    >
+                      {link.text}
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+          </SideWrapper>
+        );
+      }}
+    </ProductConsumer>
+  );
 };
+
+const SideWrapper = styled.nav`
+  position: fixed;
+  top: 77px;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: var(--mainGrey);
+  z-index: 1;
+  border-right: 3px solid var(--primaryColor);
+  transition: var(--mainTransition);
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+`;
