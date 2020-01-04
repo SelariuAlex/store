b342e11ac664ec1459ff0cb4a675a6bda5daa288
Selariu Alexandru <Selalex_gd@yahoo.com>
2020-01-04 11:24:12 +0200

    heropage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,13 @@
 import React from "react";
-import { ProductConsumer } from "../context";
+import { Hero } from "../components/Hero";
+import { Link } from "react-router-dom";
 
 export const HomePage = () => {
   return (
     <div>
-      <ProductConsumer>{value => <h1>home</h1>}</ProductConsumer>
+      <Hero title="CPUs" max="true">
+        <Link to="/products">Our products</Link>
+      </Hero>
     </div>
   );
 };
