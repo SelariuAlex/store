f54ffcba32ecefe9e176d289a4ec4ce664b17785
Selariu Alexandru <Selalex_gd@yahoo.com>
2020-01-07 12:44:48 +0200

    contact page

---
+++ src/components/ContactPage/Contact.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { Title } from "../Title";
+
+export const Contact = () => {
+  return (
+    <section className="py-5">
+      <div className="row">
+        <div className="col-10 mx-auto col-md-6 my-3">
+          <Title title="Contact Us" />
+          <form className="mt-5">
+            <div className="form-group">
+              <input
+                type="text"
+                name="firstName"
+                className="form-control"
+                placeholder="First Name"
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="email"
+                name="email"
+                className="form-control"
+                placeholder="email@email.com"
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="text"
+                name="subject"
+                className="form-control"
+                placeholder="Subject"
+              />
+            </div>
+            <div className="form">
+              <textarea
+                name="message"
+                className="form-control"
+                placeholder="your message"
+                rows="10"
+              ></textarea>
+            </div>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+};
--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,5 +1,13 @@
 import React from "react";
+import { Hero } from "../components/Hero";
+import { Contact } from "../components/ContactPage/Contact";
+import contactBcg from "../images/contactBcg.jpeg";
 
 export const ContactPage = () => {
-  return <div>Contact</div>;
+  return (
+    <div>
+      <Hero img={contactBcg}></Hero>
+      <Contact />
+    </div>
+  );
 };
