1cec6c2d0efb07c0b61218582a0a1c24bc644b31
Selariu Alexandru <Selalex_gd@yahoo.com>
2020-01-08 00:03:49 +0200

    footer

---
+++ src/context/socialData.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { FaLinkedin, FaTwitter } from "react-icons/fa";
+
+export const socialData = [
+  {
+    id: 1,
+    icon: <FaLinkedin className="icon" />,
+    url: "https://www.linkedin.com/in/alexandru-selariu-3a1262183/"
+  },
+  {
+    id: 1,
+    icon: <FaTwitter className="icon" />,
+    url: "https://twitter.com/AlexandruSelar1"
+  }
+];
